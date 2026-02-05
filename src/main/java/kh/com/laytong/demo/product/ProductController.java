d523b1d6ac0429b4a5d28231356f10543d846382
laytonglee <laytongly@gmail.com>
2026-02-05 23:44:22 +0700

    Adjust product service

--- src/main/java/kh/com/laytong/demo/product/ProductController.java
+++ src/main/java/kh/com/laytong/demo/product/ProductController.java
@@ -1,6 +1,7 @@
 package kh.com.laytong.demo.product;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,10 +10,10 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
-@RequiredArgsConstructor
 public class ProductController {
 
-    private final ProductService productService;
+    @Autowired
+    private ProductService productService;
 
 
     @PostMapping
